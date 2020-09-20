74133d8b75b80e7949229f5d8170947ae04ddab8
Laura Petrola <laupetrola@gmail.com>
2020-09-20 16:36:10 -0300

    adicionando os modais

---
+++ src/main/java/view/Atualizar_vendedor.java
@@ -0,0 +1,96 @@
+package view;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Atualizar_vendedor extends JPanel {
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+
+	/**
+	 * Create the panel.
+	 */
+	public Atualizar_vendedor() {
+		setBackground(Color.WHITE);
+		setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(102, 206, 214));
+		panel.setBounds(0, 0, 551, 53);
+		add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblAdicionarProduto = new JLabel("ADICIONAR PRODUTO");
+		lblAdicionarProduto.setForeground(Color.WHITE);
+		lblAdicionarProduto.setBounds(161, 19, 308, 15);
+		lblAdicionarProduto.setFont(new Font("Fira Code", Font.PLAIN, 20));
+		panel.add(lblAdicionarProduto);
+		
+		JLabel lblQ = new JLabel("NOME:");
+		lblQ.setForeground(Color.DARK_GRAY);
+		lblQ.setBounds(28, 98, 146, 14);
+		lblQ.setFont(new Font("Fira Code", Font.PLAIN, 13));
+		add(lblQ);
+		
+		JLabel lblQuantidade = new JLabel("LOGIN:");
+		lblQuantidade.setForeground(Color.DARK_GRAY);
+		lblQuantidade.setBounds(28, 216, 93, 14);
+		lblQuantidade.setFont(new Font("Fira Code", Font.PLAIN, 13));
+		add(lblQuantidade);
+		
+		JLabel lblPreco = new JLabel("SENHA:");
+		lblPreco.setForeground(Color.DARK_GRAY);
+		lblPreco.setBounds(329, 98, 55, 14);
+		lblPreco.setFont(new Font("Fira Code", Font.PLAIN, 13));
+		add(lblPreco);
+		
+		JLabel lblNewLabel = new JLabel("CONFIRMAR SENHA:");
+		lblNewLabel.setForeground(Color.DARK_GRAY);
+		lblNewLabel.setBounds(329, 216, 155, 14);
+		lblNewLabel.setFont(new Font("Fira Code", Font.PLAIN, 13));
+		add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("ATUALIZAR");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setForeground(Color.DARK_GRAY);
+		btnNewButton.setBackground(new Color(102, 206, 214));
+		btnNewButton.setBounds(403, 305, 109, 39);
+		btnNewButton.setFont(new Font("Fira Code", Font.PLAIN, 13));
+		add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(28, 242, 193, 26);
+		add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(325, 242, 187, 26);
+		add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(325, 124, 187, 26);
+		add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(28, 124, 187, 26);
+		add(textField_3);
+		
+
+	}
+}
--- src/main/java/view/Vendedor_inicial.java
+++ src/main/java/view/Vendedor_inicial.java
@@ -1,14 +1,102 @@
 package view;
 
 import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.UIManager;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Vendedor_inicial extends JPanel {
+	
+	private JTextField Nome;
+	private JTextField textField;
 
 	/**
 	 * Create the panel.
 	 */
 	public Vendedor_inicial() {
+		setBackground(new Color(0, 0, 0));
+		setLayout(null);
+		this.setBounds(0, 0, 768, 580);
+		
+		JPanel PainelInfo = new JPanel();
+		PainelInfo.setBackground(new Color(255, 255, 255));
+		PainelInfo.setBounds(206, 0, 562, 580);
+		add(PainelInfo);
+		PainelInfo.setLayout(null);
+		
+		JLabel InformacoesLabel = new JLabel("Informa\u00E7\u00F5es");
+		InformacoesLabel.setFont(new Font("Fira Code", Font.PLAIN, 24));
+		InformacoesLabel.setForeground(new Color(51,51,51));
+		InformacoesLabel.setBounds(10, 23, 182, 30);
+		PainelInfo.add(InformacoesLabel);
+		
+		JPanel PainelInterno = new JPanel();
+		PainelInterno.setBackground(new Color(237,237,237));
+		PainelInterno.setBounds(10, 74, 524, 428);
+		PainelInfo.add(PainelInterno);
+		PainelInterno.setLayout(null);
+		
+		Nome = new JTextField();
+		Nome.setBounds(200, 106, 271, 27);
+		PainelInterno.add(Nome);
+		Nome.setColumns(10);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(200, 164, 271, 27);
+		PainelInterno.add(textField);
+		
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setFont(new Font("Fira Code", Font.PLAIN, 18));
+		lblNome.setForeground(new Color(129,129,129));
+		lblNome.setBounds(133, 112, 55, 16);
+		PainelInterno.add(lblNome);
+		
+		JLabel lblCpf = new JLabel("CPF:");
+		lblCpf.setFont(new Font("Fira Code", Font.PLAIN, 18));
+		lblCpf.setForeground(new Color(129,129,129));
+		lblCpf.setBounds(133, 167, 55, 16);
+		
+		PainelInterno.add(lblCpf);
+		JPanel panelButtons = new JPanel();
+		panelButtons.setBackground(new Color(102, 206, 214));
+		panelButtons.setBounds(0, 0, 208, 580);
+		add(panelButtons);
+		panelButtons.setLayout(null);
+		
+		JLabel TipoUsuario = new JLabel("VENDEDOR");
+		TipoUsuario.setFont(new Font("Fira Code", Font.BOLD, 20));
+		TipoUsuario.setForeground(new Color(255,255,255));
+		TipoUsuario.setEnabled(true);
+		TipoUsuario.setBounds(56, 22, 146, 26);
+		panelButtons.add(TipoUsuario);
+		
+		JButton btnConVendas = new JButton("VENDER");
+		btnConVendas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnConVendas.setBackground(Color.WHITE);
+		btnConVendas.setFont(new Font("Fira Code", Font.PLAIN, 10));
+		btnConVendas.setBounds(38, 96, 142, 52);
+		panelButtons.add(btnConVendas);
+		
+		JButton btConEstoque = new JButton("CONSULTAR ESTOQUE");
+		btConEstoque.setBackground(Color.WHITE);
+		btConEstoque.setFont(new Font("Fira Code", Font.PLAIN, 10));
+		btConEstoque.setBounds(34, 183, 146, 52);
+		panelButtons.add(btConEstoque);
+		
+		JButton btSair = new JButton("SAIR");
+		btSair.setFont(new Font("Fira Code", Font.PLAIN, 14));
+		btSair.setBackground(Color.WHITE);
+		btSair.setBounds(34, 425, 146, 52);
+		panelButtons.add(btSair);
 
 	}
-
 }
