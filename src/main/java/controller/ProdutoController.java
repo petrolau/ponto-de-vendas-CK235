4989c8a3afd58d4260740966d86e4ad59b8231aa
VicMisael <misael1805@gmail.com>
2020-10-04 19:31:46 -0300

    Finished the initial controller version

--- src/main/java/controller/ProdutoController.java
+++ src/main/java/controller/ProdutoController.java
@@ -1,10 +1,36 @@
 package controller;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 import model.Produto;
+import model.Usuario;
+import util.DBUtil;
 
 public class ProdutoController {
 
-	public void InserirProduto(Produto p) {
-		
+	public static void InserirProduto(Produto p) {
+		p.setId(null);
+		EntityManager em = DBUtil.getEntityManager();
+		em.getTransaction().begin();
+		em.persist(p);
+		em.getTransaction().commit();
+		DBUtil.closeEntityManager(em);
+	}
+	public static List<Produto> getProdutoList(){
+		EntityManager em=DBUtil.getEntityManager();
+		TypedQuery<Produto> q=em.createQuery("FROM Produto",Produto.class);
+		List<Produto> ps=q.getResultList();
+		DBUtil.closeEntityManager(em);
+		return ps;
+	}
+	public static void atualizarProduto(Produto p) {
+		EntityManager em=DBUtil.getEntityManager();
+		em.getTransaction().begin();
+		em.persist(p);
+		em.getTransaction().commit();
+		DBUtil.closeEntityManager(em);
 	}
 }
--- src/main/java/controller/VendaController.java
+++ src/main/java/controller/VendaController.java
@@ -1,5 +1,34 @@
 package controller;
 
-public class VendaController {
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import model.Venda;
+import util.DBUtil;
+
+public class VendaController {
+	public static void InserirVenda(Venda v) {
+		v.setId(null);
+		EntityManager em = DBUtil.getEntityManager();
+		em.getTransaction().begin();
+		em.persist(v);
+		em.getTransaction().commit();
+		DBUtil.closeEntityManager(em);
+	}
+	public static List<Venda> getVendasList(){
+		EntityManager em=DBUtil.getEntityManager();
+		TypedQuery<Venda> q=em.createQuery("FROM Venda",Venda.class);
+		List<Venda> vs=q.getResultList();
+		DBUtil.closeEntityManager(em);
+		return vs;
+	}
+	public static void deletarVenda(Venda v) {
+		EntityManager em = DBUtil.getEntityManager();
+		em.getTransaction().begin();
+		em.remove(v);
+		em.getTransaction().commit();
+		DBUtil.closeEntityManager(em);
+	}
 }
